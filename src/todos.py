60098a750aeceb4c3c9572f38044fc0b8ab7c203
Michal Turek <mixalturek@users.sf.net>
2013-05-31 13:25:42 +0200

    Output - Code for output updated to use formatters. - HTML formatter implemented. - Output to stdout is enabled only if no other output is specified.

--- src/todos.py
+++ src/todos.py
@@ -30,29 +30,14 @@ import re
 ###############################################################################
 #### Configuration, default values
 
-VERSION = '0.1.0'
+TODOS_VERSION = '0.1.0'
+XML_VERSION = '0.1.0'
 
 COMMENTS = ['#', '//', '/*']
 PATTERNS = [r'\bTODO\b', r'\bFIXME\b']
 SUPPRESSED = ['.git', '.svn', 'CVS']
 DIRECTORIES = ['.']
 NUM_LINES = 1
-
-
-XML_HEADER = '''<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<Todos>
-	<Version version="{0}">
-	<Comments>'''.format(VERSION)
-
-XML_COMMENT='''
-		<Comment pattern="{0}" file="{1}" line="{2}">
-			{3}
-		</Comment>
-'''
-
-XML_FOOTER = '''	</Comments>
-</Todos>
-'''
 
 
 ###############################################################################
@@ -64,6 +49,199 @@ class Comment:
 		self.file = file
 		self.pos = pos
 		self.lines = lines
+
+
+###############################################################################
+####
+
+# TODO: summary in TXT
+
+class TxtFormatter:
+	MULTILINE_DELIMITER = '--'
+
+
+	def __init__(self, multiline):
+		self.multiline = multiline
+
+
+	def getType(self):
+		return 'TXT'
+
+
+	def writeHeader(self, outStream):
+		# Empty
+		pass
+
+
+	def writeComments(self, outStream, comments):
+		if self.multiline:
+			print >> outStream, self.MULTILINE_DELIMITER
+
+		for comment in comments:
+			pos = comment.pos
+
+			for line in comment.lines:
+				print >> outStream, '{0}:{1}: {2}'.format(comment.file, pos, line)
+				pos += 1
+
+			if self.multiline:
+				print >> outStream, self.MULTILINE_DELIMITER
+
+
+	def writeFooter(self, outStream):
+		# Empty
+		pass
+
+
+###############################################################################
+####
+
+# TODO: summary in XML
+
+class XmlFormatter:
+	def __init__(self):
+		# Empty
+		pass
+
+
+	def getType(self):
+		return 'XML'
+
+
+	def writeHeader(self, outStream):
+		print >> outStream, '<?xml version="1.0" encoding="utf-8" standalone="yes"?>'
+		print >> outStream, '<Todos>'
+		print >> outStream, '\t<Version todos="{0}" format="{1}">'.format(TODOS_VERSION, XML_VERSION)
+		print >> outStream, '\t<Comments>'
+
+
+	def writeComments(self, outStream, comments):
+		for comment in comments:
+			print >> outStream, '\t\t<Comment pattern="{0}" file="{1}" line="{2}">'.format(
+					self.xmlSpecialChars(comment.pattern),
+					self.xmlSpecialChars(comment.file),
+					comment.pos)
+
+			for line in comment.lines:
+				print >> outStream, '\t\t\t{0}'.format(self.xmlSpecialChars(line))
+
+			print >> outStream, '\t\t</Comment>'
+
+
+	def writeFooter(self, outStream):
+		print >> outStream, '\t</Comments>'
+		print >> outStream, '</Todos>'
+
+
+	def xmlSpecialChars(self, text):
+		ret = text
+		ret = ret.replace('&', '&amp;')
+		ret = ret.replace('"', '&quot;')
+		ret = ret.replace('<', '&lt;')
+		ret = ret.replace('>', '&gt;')
+		return ret
+
+
+###############################################################################
+####
+
+# TODO: summary in HTML
+
+class HtmlFormatter:
+	def __init__(self):
+		# Empty
+		pass
+
+
+	def getType(self):
+		return 'HTML'
+
+
+	def writeHeader(self, outStream):
+		print >> outStream, '''<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta http-equiv="content-language" content="en" />
+
+<title>Comments - todos</title>
+
+<style type="text/css" media="all">
+* { margin: 0; padding: 0; }
+
+body
+{
+	margin: 2em; padding: 0px;
+	background-color: white; color: black;
+	font-family: Verdana, "Bitstream Vera Sans", Geneva, Arial, sans-serif;
+	font-size: 10pt; line-height: 1.6em;
+}
+
+pre         { line-height: 1.1em; margin: 0; margin: 0.2em 0 0.2em 0; }
+a:hover     { color: blue; }
+
+table       { margin-top: 1.5em; max-width: 100%; }
+th          { background-color: #AFB3CC; }
+th, td      { vertical-align: top; padding: 0.2em 0.5em 0.2em 0.5em; }
+tr          { background-color: #D0D0EE; }
+tr:hover    { background-color: #C0C0FF; }
+
+#footer     { font-size: 9pt; margin-top: 3em; border-top: 1px solid silver; color: gray; }
+</style>
+
+</head>
+
+<body>
+
+<h1>Comments</h1>
+'''
+
+
+	def writeComments(self, outStream, comments):
+		print >> outStream, '<table>'
+		print >> outStream, '<thead>'
+		print >> outStream, '<tr>'
+		print >> outStream, '<th>File</th>'
+		print >> outStream, '<th>Line</th>'
+		print >> outStream, '<th>Pattern</th>'
+		print >> outStream, '<th>Content</th>'
+		print >> outStream, '</tr>'
+		print >> outStream, '</thead>'
+		print >> outStream, '<tbody>'
+		print >> outStream
+
+		for comment in comments:
+			print >> outStream, '<tr>'
+			print >> outStream, '<td><a href="{0}">{0}</a></td>'.format(self.htmlSpecialChars(comment.file))
+			print >> outStream, '<td>{0}</td>'.format(comment.pos)
+			print >> outStream, '<td>{0}</td>'.format(self.htmlSpecialChars(comment.pattern))
+			print >> outStream, '<td><pre>'
+			for line in comment.lines:
+				print >> outStream, '{0}'.format(self.htmlSpecialChars(line))
+			print >> outStream, '</pre></td>'
+			print >> outStream, '</tr>'
+			print >> outStream
+
+		print >> outStream, '</tbody>'
+		print >> outStream, '</table>'
+
+
+	def writeFooter(self, outStream):
+		print >> outStream, '<p id="footer">This page was generated using <a href="http://todos.sourceforge.net/">todos</a> tool.</p>'
+		print >> outStream, '</body>'
+		print >> outStream, '</html>'
+
+
+	def htmlSpecialChars(self, text):
+		ret = text
+		ret = ret.replace('&', '&amp;')
+		ret = ret.replace('"', '&quot;')
+		ret = ret.replace('<', '&lt;')
+		ret = ret.replace('>', '&gt;')
+		return ret
 
 
 ###############################################################################
@@ -226,92 +404,44 @@ class CommentsSearch:
 
 
 	def output(self):
-		self.writeTxt()
-		self.writeXml()
-		self.writeHtml()
+		outputWritten = False
+
+		if self.parameters.outTxt is not None:
+			self.outputDataToFile(self.parameters.outTxt, TxtFormatter(self.parameters.numLines > 1))
+			outputWritten = True
+
+		if self.parameters.outXml is not None:
+			self.outputDataToFile(self.parameters.outXml, XmlFormatter())
+			outputWritten = True
+
+		if self.parameters.outHtml is not None:
+			self.outputDataToFile(self.parameters.outHtml, HtmlFormatter())
+			outputWritten = True
+
+		# Use stdout if no output method is specified explicitly
+		if outputWritten == False:
+			self.outputData(sys.stdout, TxtFormatter(self.parameters.numLines > 1))
 
 
-	def writeTxt(self):
-		if self.parameters.outTxt is None:
-			return
-
-		self.verbose('Writing TXT output: ' + self.parameters.outTxt)
-
-		if self.parameters.outTxt == 'stdout':
-			outFile = sys.stdout
-		else:
-			if os.path.exists(self.parameters.outTxt) and not self.parameters.force:
-				print >> sys.stderr, 'File exists, use force parameter to override:', self.parameters.outTxt
-				return
-
-			try:
-				outFile = open(self.parameters.outTxt, 'w')
-			except IOError, e:
-				print >> sys.stderr, 'File opening failed:', e
-				return
-
-		if self.parameters.numLines > 1:
-			print >> outFile, '--'
-
-		for comment in self.comments:
-			pos = comment.pos
-
-			for line in comment.lines:
-				print >> outFile, comment.file + ':' + str(pos), line
-				pos += 1
-
-			if self.parameters.numLines > 1:
-				print >> outFile, '--'
-
-		if self.parameters.outTxt != 'stdout':
-			outFile.close()
+	def outputData(self, outStream, formatter):
+		formatter.writeHeader(outStream)
+		formatter.writeComments(outStream, self.comments)
+		formatter.writeFooter(outStream)
 
 
-	def xmlSpecialChars(self, text):
-		ret = text
-		ret = ret.replace('&', '&amp;')
-		ret = ret.replace('"', '&quot;')
-		ret = ret.replace('<', '&lt;')
-		ret = ret.replace('>', '&gt;')
-		return ret
+	def outputDataToFile(self, path, formatter):
+		self.verbose('Writing {0} output: {1}'.format(formatter.getType(), path))
 
-
-	def writeXml(self):
-		if self.parameters.outXml is None:
-			return
-
-		self.verbose('Writing XML output: ' + self.parameters.outXml)
-
-		if os.path.exists(self.parameters.outXml) and not self.parameters.force:
-			print >> sys.stderr, 'File exists, use force parameter to override:', self.parameters.outXml
+		if os.path.exists(path) and not self.parameters.force:
+			print >> sys.stderr, 'File exists, use force parameter to override:', path
 			return
 
 		try:
-			outFile = open(self.parameters.outXml, 'w')
+			with open(path, 'w') as outStream:
+				self.outputData(outStream, formatter)
 		except IOError, e:
-			print >> sys.stderr, 'File opening failed:', e
+			print >> sys.stderr, 'Output failed:', e
 			return
-
-		print >> outFile, XML_HEADER
-
-		for comment in self.comments:
-			print >> outFile, XML_COMMENT.format(
-					 self.xmlSpecialChars(comment.pattern),
-					 self.xmlSpecialChars(comment.file),
-					 comment.pos,
-					 self.xmlSpecialChars('\n\t\t\t'.join(comment.lines)))
-
-		print >> outFile, XML_FOOTER
-		outFile.close()
-
-
-	def writeHtml(self):
-		if self.parameters.outHtml is None:
-			return
-
-		self.verbose('Writing HTML output: ' + self.parameters.outHtml)
-		# FIXME: add implementation
-		pass
 
 
 ###############################################################################
@@ -327,7 +457,7 @@ def parseCommandLineArguments():
 			'-V', '--version',
 			help='show version and exit',
 			action='version',
-			version='%(prog)s ' + VERSION)
+			version='%(prog)s ' + TODOS_VERSION)
 
 	parser.add_argument(
 			'-v', '--verbose',
@@ -382,8 +512,6 @@ def parseCommandLineArguments():
 
 	parser.add_argument(
 			'-o', '--out-txt',
-			nargs='?',
-			const='stdout',
 			metavar='TXT',
 			dest='outTxt',
 			help='the output text file; standard output will be used if the path is not specified')
